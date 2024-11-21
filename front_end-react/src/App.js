a0227a867009869a8c5e28f7c0b7f77e12b484b6
0940185778 <biniyamassefa648@gmail.com>
2024-11-22 01:35:22 +0300

    feat:  add features

--- front_end-react/src/App.js
+++ front_end-react/src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./containers";
+import { NavBar } from "./components";
+import "./App.scss";
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="app">
+      <NavBar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
