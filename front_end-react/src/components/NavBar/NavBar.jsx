39830107276d5dbafcb70d08a45fabb8f94d5215
0940185778 <biniyamassefa648@gmail.com>
2024-11-22 17:27:34 +0300

    feat: make the navbar page

--- front_end-react/src/components/NavBar/NavBar.jsx
+++ front_end-react/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./NavBar.scss";
 
+import images from "../../constants/images";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+        <ul>
+          {["home", "about", "work", "skills", "contact"].map((items) => (
+            <li key={`link-${items}`}>
+              <div />,<a href={`#${items}`}>{items}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
