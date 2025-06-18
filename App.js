32ddb437fc028ff33bf0e3cb80ddbcace6b4c07b
Biniyam <biniyamassefa648@gmail.com>
2025-06-18 09:56:22 +0300

    add app .js

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./containers";
+import { NavBar } from "./components";
+import "./App.scss";
+function App() {
+  return (
+    <div className="app">
+      <NavBar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
